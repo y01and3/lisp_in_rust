a752cdbfb17b1968aa0feed83aac1fa8081d6bbb
y01and3 <1791280644@qq.com>
2023-11-20 15:56:18 +0800

    change back box

--- src/lisp/eval/expr.rs
+++ src/lisp/eval/expr.rs
@@ -1,10 +1,10 @@
 use super::{list::List, num::number::Number};
-use std::{fmt::Display, rc::Rc};
+use std::fmt::Display;
 
 pub enum Expr {
     Symbol(String),
     Number(Number),
-    List(Rc<List>),
+    List(Box<List>),
     Nil,
 }
 
--- src/lisp/eval/list.rs
+++ src/lisp/eval/list.rs
@@ -1,9 +1,9 @@
 use super::expr::Expr;
-use std::{fmt::Display, rc::Rc};
+use std::fmt::Display;
 
 pub struct List {
     data: Expr,
-    pointer: Option<Rc<List>>,
+    pointer: Option<Box<List>>,
 }
 
 impl List {
@@ -28,13 +28,13 @@ pub fn cons(obj1: Expr, obj2: Expr) -> List {
     let mut list = List::new(obj1);
     match obj2 {
         Expr::List(cdr) => {
-            list.pointer = Some(Rc::clone(&cdr));
+            list.pointer = Some(cdr);
         }
         Expr::Nil => {
             list.pointer = None;
         }
         _ => {
-            list.pointer = Some(Rc::new(List::new(obj2)));
+            list.pointer = Some(Box::new(List::new(obj2)));
         }
     }
     list
