247452ffecaf7e3d8eb90990a0785ceaaf8e8d93
y01and3 <1791280644@qq.com>
2024-01-04 16:51:07 +0800

    reconstruct number types

--- src/eval/num/real.rs
+++ src/eval/num/real.rs
@@ -1,115 +1,43 @@
-use super::{float::Float, int::Int, ratio::Ratio};
-use std::fmt::Display;
+use super::{float::Float, int::Int};
 
-#[derive(Clone, Debug, PartialEq)]
-pub enum Real {
-    Int(Int),
-    Float(Float),
-    Ratio(Ratio),
-}
-
-impl Display for Real {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Real::Int(i) => write!(f, "{}", i),
-            Real::Float(float) => write!(f, "{}", float),
-            Real::Ratio(r) => write!(f, "{}", r),
-        }
-    }
-}
-
-impl Real {
-    fn to_float(&self) -> Float {
-        match self {
-            Real::Int(i) => Float::new(i.value() as f64),
-            Real::Float(f) => Float::new(f.value()),
-            Real::Ratio(r) => Float::new(r.value()),
-        }
-    }
-    fn truncate(&self) -> (Real, Real) {
+pub trait Real {
+    fn to_float(&self) -> Float;
+    fn truncate(&self) -> (Int, Float) {
         let number = self.to_float().value();
         let quotient = number.trunc() as i64;
         let remainder = number - quotient as f64;
-        if remainder == remainder.floor() {
-            (
-                Real::Int(Int::new(quotient)),
-                Real::Int(Int::new(remainder as i64)),
-            )
-        } else {
-            (
-                Real::Int(Int::new(quotient)),
-                Real::Float(Float::new(remainder)),
-            )
-        }
+        (Int::new(quotient), Float::new(remainder))
     }
-    fn floor(&self) -> (Real, Real) {
+    fn floor(&self) -> (Int, Float) {
         let number = self.to_float().value();
         let quotient = number.floor() as i64;
         let remainder = number - quotient as f64;
-        if remainder == remainder.floor() {
-            (
-                Real::Int(Int::new(quotient)),
-                Real::Int(Int::new(remainder as i64)),
-            )
-        } else {
-            (
-                Real::Int(Int::new(quotient)),
-                Real::Float(Float::new(remainder)),
-            )
-        }
+        (Int::new(quotient), Float::new(remainder))
     }
-    fn ceiling(&self) -> (Real, Real) {
+    fn ceiling(&self) -> (Int, Float) {
         let number = self.to_float().value();
         let quotient = number.ceil() as i64;
         let remainder = number - quotient as f64;
-        if remainder == remainder.floor() {
-            (
-                Real::Int(Int::new(quotient)),
-                Real::Int(Int::new(remainder as i64)),
-            )
-        } else {
-            (
-                Real::Int(Int::new(quotient)),
-                Real::Float(Float::new(remainder)),
-            )
-        }
+        (Int::new(quotient), Float::new(remainder))
     }
-    fn round(&self) -> (Real, Real) {
+    fn round(&self) -> (Int, Float) {
         let number = self.to_float().value();
         let quotient = number.round() as i64;
         let remainder = number - quotient as f64;
-        if remainder == remainder.floor() {
-            (
-                Real::Int(Int::new(quotient)),
-                Real::Int(Int::new(remainder as i64)),
-            )
-        } else {
-            (
-                Real::Int(Int::new(quotient)),
-                Real::Float(Float::new(remainder)),
-            )
-        }
+        (Int::new(quotient), Float::new(remainder))
     }
-    fn modulus(&self, rhs: &Self) -> Real {
+    fn modulus(&self, rhs: &dyn Real) -> Float {
         let divisor = rhs.to_float().value();
         let number = self.to_float().value();
         let quotient = (number / divisor).floor() as i64;
         let remainder = number - quotient as f64;
-        if remainder == remainder.floor() {
-            Real::Int(Int::new(remainder as i64))
-        } else {
-            Real::Float(Float::new(remainder))
-        }
+        Float::new(remainder)
     }
-    fn rem(&self, rhs: &Self) -> Real {
+    fn rem(&self, rhs: &dyn Real) -> Float {
         let divisor = rhs.to_float().value();
         let number = self.to_float().value();
         let quotient = (number / divisor).trunc() as i64;
         let remainder = number - quotient as f64;
-        if remainder == remainder.floor() {
-            Real::Int(Int::new(remainder as i64))
-        } else {
-            Real::Float(Float::new(remainder))
-        }
+        Float::new(remainder)
     }
 }
